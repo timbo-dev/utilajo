1580d7b7327b4e96d2083c3a0931d2b9ecd6c402
timbo-dev <dev@nathanrocha.com.br>
2023-09-04 11:24:35 -0300

    🧪 test(either/ok): add ok missing tests
    This coverage the creation of `EitherOk` instance and it types.

---
+++ packages/either/src/core/spec/ok.spec.ts
@@ -0,0 +1,41 @@
+import { Either } from '../either.type';
+import { EitherErr } from '../err';
+import { EitherOk, ok } from '../ok';
+
+class SutError extends Error {
+    constructor() {
+        super('error message');
+        this.name = this.constructor.name;
+    }
+}
+
+it('should be able to create a new EitherOk instance', () => {
+    const sut = ok('ok message');
+
+    expect(sut).toBeInstanceOf(EitherOk);
+
+    expect(sut.isErr()).toBe(false);
+    expect(sut.isOk()).toBe(true);
+
+    expect(sut.getValue()).toBe('ok message');
+});
+
+it('should be return EitherOk instance type', () => {
+    function sutFunction(): Either<SutError, string> {
+        return ok('ok message');
+    }
+
+    const sut = sutFunction();
+
+    expectTypeOf(sut).toEqualTypeOf<EitherErr<SutError> | EitherOk<string>>();
+
+    if (sut.isOk()) {
+        expectTypeOf(sut).toEqualTypeOf<EitherOk<string>>();
+        expectTypeOf(sut.getValue()).toEqualTypeOf<string>();
+    }
+    else {
+        expectTypeOf(sut).toEqualTypeOf<EitherErr<SutError>>();
+        expectTypeOf(sut.getValue()).toEqualTypeOf<SutError>();
+    }
+});
+
